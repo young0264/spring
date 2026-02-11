3c363448d53de86cd13705ae2e1e012904251531
Euiyoung Nam <96028167+young0264@users.noreply.github.com>
2026-02-11 18:25:16 +0900

    update servlet response - using controller java class(not front-controller) - include name parameter

--- src/main/java/spring/deep/DeepApplication.java
+++ src/main/java/spring/deep/DeepApplication.java
@@ -1,17 +1,16 @@
 package spring.deep;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import spring.deep.controller.HelloController;
 
 import java.io.IOException;
 
@@ -20,21 +19,32 @@ public class DeepApplication {
 
 	public static void main(String[] args) {
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		System.out.println("======= Hello DeepApplication =======");
 		// Add Servlet to servlet-container
 		// 익명클래스 활용
 		// 웹프로그래밍은 요청을 받아서 응답을 만들어 내는 것.
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+			HelloController helloController = new HelloController();
 
-					// (1)status code, (2)content-type, (2)body
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello Created Servlet, and " + name);
+			servletContext.addServlet("frontController", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+					if (req.getRequestURI().equals("/hello")
+							&& req.getMethod().equals(HttpMethod.GET.name())) {
+
+						String name = req.getParameter("name");
+						String ret = helloController.hello(name);
+
+						// (1)status code, (2)content-type, (2)body
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("ret: " + ret);
+					} else if (req.getRequestURI().equals("/user")) {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					} else {
+					}
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*"); //servlet container에 매핑정보 등록
         });
 		webServer.start();
 	}
--- src/main/java/spring/deep/controller/HelloController.java
+++ src/main/java/spring/deep/controller/HelloController.java
@@ -1,12 +1,9 @@
 package spring.deep.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+// Simple java class without any Spring
 public class HelloController {
-    @GetMapping("/hello")
     public String hello(String name) {
+        System.out.println("======= Hello GetMapping =======");
         return "Hello " + name;
     }
 }
