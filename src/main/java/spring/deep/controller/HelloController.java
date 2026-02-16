0ef64718ad9d1cfd97cfc41c8505ecdb84f52062
Euiyoung Nam <96028167+young0264@users.noreply.github.com>
2026-02-16 12:49:08 +0900

    refactor: simplify HelloController by removing unused ApplicationContext test: add unit tests for HelloController

--- src/main/java/spring/deep/controller/HelloController.java
+++ src/main/java/spring/deep/controller/HelloController.java
@@ -7,27 +7,16 @@ import spring.deep.service.HelloService;
 
 import java.util.Objects;
 
-// Simple java class without any Spring
 @RestController
 public class HelloController {
-//public class HelloController implements ApplicationContextAware {
     private final HelloService helloService;
-    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.applicationContext = applicationContext;
-        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
-
-//    @Override
-//    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-//        System.out.println(applicationContext);
-//        this.applicationContext = applicationContext;
-//    }
 }
---
+++ src/test/java/spring/deep/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package spring.deep;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import spring.deep.controller.HelloController;
+
+public class HelloControllerTest {
+
+    @Test
+    void helloController() {
+        HelloController helloController = new HelloController(name -> name);
+        String ret = helloController.hello("TEST");
+        Assertions.assertThat(ret).isEqualTo("TEST");
+    }
+
+    @Test
+    void failHelloController() {
+        HelloController helloController = new HelloController(name -> name);
+        Assertions.assertThatThrownBy(() -> helloController.hello(null))
+                .isInstanceOf(NullPointerException.class);
+    }
+}
