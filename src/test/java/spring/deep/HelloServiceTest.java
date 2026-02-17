5b0017f166aa7e051359061aadc51731b637ee3b
Euiyoung <96028167+young0264@users.noreply.github.com>
2026-02-17 13:11:11 +0900

    feat: add custom annotations for unit testing

--- src/test/java/spring/deep/HelloServiceTest.java
+++ src/test/java/spring/deep/HelloServiceTest.java
@@ -6,8 +6,25 @@ import spring.deep.controller.HelloController;
 import spring.deep.service.HelloDecorator;
 import spring.deep.service.SimpleHelloService;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@UnitTest
+@interface FastUnitTest {
+}
+
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD }) // @Test 내 확인
+@Retention(RetentionPolicy.RUNTIME)
+@Test
+@interface UnitTest {
+}
+
 public class HelloServiceTest {
-    @Test
+    @UnitTest
     void simpleServiceTest() {
         SimpleHelloService helloService = new SimpleHelloService();
         String ret = helloService.sayHello("Test");
