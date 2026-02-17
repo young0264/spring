f6a6786e87e460c20adb79bdfe26d87c30c389e0
Euiyoung <96028167+young0264@users.noreply.github.com>
2026-02-17 15:40:49 +0900

    feat: implement custom auto-configuration with @CustomAutoConfiguration and dynamic import selection

--- src/main/java/spring/config/CustomAutoConfigImportSelector.java
+++ src/main/java/spring/config/CustomAutoConfigImportSelector.java
@@ -1,14 +1,29 @@
 package spring.config;
 
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CustomAutoConfigImportSelector implements DeferredImportSelector {
+public class CustomAutoConfigImportSelector implements DeferredImportSelector, BeanClassLoaderAware {
+    private ClassLoader classLoader;
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "spring.config.autoconfig.DispatcherServletConfig",
-                "spring.config.autoconfig.TomcatWebServerConfig"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+        ImportCandidates.load(CustomAutoConfiguration.class, classLoader)
+                .forEach(candidate -> autoConfigs.add(candidate));
+
+        // type safe
+        return autoConfigs.toArray(new String[0]);
+//        return Arrays.copyOf(autoConfigs.toArray(), autoConfigs.size(), String[].class);
+//        return autoConfigs.stream().toArray(String[]::new);
     }
 }
