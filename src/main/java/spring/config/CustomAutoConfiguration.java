6f6dfc96379406333e96f429ab2d8be7e410beaa
Euiyoung <96028167+young0264@users.noreply.github.com>
2026-02-17 17:00:41 +0900

    feat: add @Configuration with proxy false mode

--- src/main/java/spring/config/CustomAutoConfiguration.java
+++ src/main/java/spring/config/CustomAutoConfiguration.java
@@ -1,5 +1,6 @@
 package spring.config;
 
+import org.springframework.context.annotation.Configuration;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,5 +8,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Configuration(proxyBeanMethods = false)
 public @interface CustomAutoConfiguration {
 }
