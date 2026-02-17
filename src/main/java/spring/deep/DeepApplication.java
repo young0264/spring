c32ca538c02e3170217e4f7d3ec23a72f3a9b031
Euiyoung <96028167+young0264@users.noreply.github.com>
2026-02-17 13:52:07 +0900

    feat: add custom configuration and annotation for Spring Boot application

--- src/main/java/spring/deep/DeepApplication.java
+++ src/main/java/spring/deep/DeepApplication.java
@@ -1,26 +1,10 @@
 package spring.deep;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.servlet.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
+import spring.deep.annotation.CustomSpringBootAnnotation;
 
-@ComponentScan
-@Configuration
+@CustomSpringBootAnnotation
 public class DeepApplication {
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
-	}
-
-	@Bean
-	public DispatcherServlet dispatcherServlet() {
-		return new DispatcherServlet();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DeepApplication.class, args);
 	}
---
+++ src/main/java/spring/deep/config/CustomConfig.java
@@ -0,0 +1,20 @@
+package spring.deep.config;
+
+import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.servlet.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+public class CustomConfig {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+}
