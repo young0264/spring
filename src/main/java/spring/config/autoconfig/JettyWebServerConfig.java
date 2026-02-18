937a71aab80961734d23311597ba6be6ee855676
Euiyoung <96028167+young0264@users.noreply.github.com>
2026-02-18 23:38:57 +0900

    feat: add Jetty web server configuration and update dependencies

--- src/main/java/spring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/spring/config/autoconfig/JettyWebServerConfig.java
@@ -1,15 +1,15 @@
 package spring.config.autoconfig;
 
+import org.springframework.boot.jetty.servlet.JettyServletWebServerFactory;
 import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.servlet.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import spring.config.CustomAutoConfiguration;
 
 @CustomAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean("tomcatServletWebServerFactory")
+public class JettyWebServerConfig {
+    @Bean("jettyServletWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
-
 }
